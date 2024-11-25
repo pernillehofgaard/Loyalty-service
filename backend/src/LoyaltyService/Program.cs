0198b5288a9067d0712a0f85317f69df476119ef
kfr <frekai@gmail.com>
2024-11-25 21:35:18 +0100

    Update project

---
+++ backend/src/LoyaltyService/Program.cs
@@ -0,0 +1,44 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/families", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new IceCash
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetFamilies")
+    .WithOpenApi();
+
+app.Run();
+
+record IceCash(DateOnly Date, int Coin, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(Coin / 0.5556);
+}
