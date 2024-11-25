4f8ef67ce517387000b074607b39d25350f0e06f
kfr <frekai@gmail.com>
2024-11-25 20:37:00 +0100

    add frontend

---
+++ frontend/ice-loyalty-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ice-loyalty-app',
+  webDir: 'dist'
+};
+
+export default config;
